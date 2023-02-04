311acad4bef29b591a15be4fb1af9d38ccec2ae1
vivekbeast <vivektarnallya@gmail.com>
2023-02-04 23:32:38 +0530

    Calculater-1st Project

---
+++ script.js
@@ -0,0 +1,40 @@
+var date = Date();
+var name = prompt("WHAT ARE YOU DOING BROTHER?");
+document.write(name + "<br>");
+ document.write(document.baseURI+"<br>")
+document.write(document.title+"<br>");
+document.write(date);
+var pars = document.getElementsByTagName("p");
+document.write(pars + "<br/>");
+document.write(pars.length);
+document.getElementById('test').innerHTML = "heeeeeeeeee!";
+console.log("this is is ");
+let string ='';
+let buttons = document.querySelectorAll('.but');
+Array.from(buttons).forEach((but)=>{
+  but.addEventListener('click', (e) =>{
+    if(e.target.innerHTML == '='){
+       string  = eval(string);
+       document.querySelector('input').value = string;
+
+    }
+    else if(e.target.innerHTML == 'C'){
+      string  = '';
+      document.querySelector('input').value = string;
+
+   }
+   else if(e.target.innerHTML == 'a'){
+    string  = '';
+    document.querySelector('input').value = string;
+
+   }
+    else{
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+    }
+  
+
+})
+})
+
